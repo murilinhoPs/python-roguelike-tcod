c8a55b8b093f65dde016413f3e040faef5f7dff4
murilinhops <murilinhorps@gmail.com>
2024-04-18 16:22:15 -0300

    Remove comments

--- main.py
+++ main.py
@@ -7,11 +7,11 @@ from actions import MovementAction, EscAction
 from input_handlers import EventHandler
 
 
-def main() -> None:  # returns nothing, um void
+def main() -> None:
     screen_width = 80
     screen_height = 50
 
-    player_x = int(screen_width / 2)  # the same as width // 2, but only if the divisor is without dot (i.e 2.0)
+    player_x = int(screen_width / 2)
     player_y = int(screen_height / 2)
 
     tileset = tcod.tileset.load_tilesheet(
@@ -20,26 +20,26 @@ def main() -> None:  # returns nothing, um void
 
     event_handler = EventHandler()
 
-    with tcod.context.new_terminal(  # Creates the screen in current context, with (com o new terminal criado...)
+    with tcod.context.new_terminal(
             screen_width,
             screen_height,
             tileset=tileset,
             title="Yet Another Roguelike Tutorial",
             vsync=True,
     ) as context:
-        root_console = tcod.Console(screen_width, screen_height, order="F")  # creates our “console” which is what
-        # we’ll be drawing to its current order to draw is [y,x], which is unintuitive. order="F" set its order to [x,y]
-        while True:  # game loop
-            root_console.print(x=player_x, y=player_y, string="@")  # prints on pos 1x1 @
+        root_console = tcod.Console(screen_width, screen_height, order="F")
 
-            context.present(root_console)  # Render the console to the window and show it
+        while True:
+            root_console.print(x=player_x, y=player_y, string="@")
 
-            root_console.clear()  # clear the previous player position, after movementAction was performed
+            context.present(root_console)
 
-            for event in tcod.event.wait():  # waits until some event is performed
+            root_console.clear()
+
+            for event in tcod.event.wait():
                 action = event_handler.dispatch(event)
                 if action is None:
-                    continue  # continue on the loop until any action was taken
+                    continue
                 if isinstance(action, MovementAction):
                     player_x += action.dx
                     player_y += action.dy
