5be854a9a1986bf5b4e714ee5ebcc12171c25ea1
murilinhops <murilinhorps@gmail.com>
2024-04-18 13:43:07 -0300

    Init step1 - player movement

---
+++ main.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+from __future__ import annotations
+
+import tcod
+
+
+def main() -> None:  # returns nothing, um void
+    screen_width = 80
+    screen_height = 50
+
+    player_x = int(screen_width / 2)  # the same as width // 2, but only if the divisor is without dot (i.e 2.0)
+    player_y = int(screen_height / 2)
+
+    tileset = tcod.tileset.load_tilesheet(
+        "./dejavu10_10_gs_tc.png", 32, 8, tcod.tileset.CHARMAP_TCOD
+    )
+
+    with tcod.context.new_terminal(  # Creates the screen in current context, with (com o new terminal criado...)
+            screen_width,
+            screen_height,
+            tileset=tileset,
+            title="Yet Another Roguelike Tutorial",
+            vsync=True,
+    ) as context:
+        root_console = tcod.Console(screen_width, screen_height, order="F")  # creates our “console” which is what
+        # we’ll be drawing to its current order to draw is [y,x], which is unintuitive. order="F" set its order to [x,y]
+        while True:  # game loop
+            root_console.print(x=player_x, y=player_y, string="@")  # prints on pos 1x1 @
+
+            context.present(root_console)  # Render the console to the window and show it
+
+            for event in tcod.event.wait():  # waits until some event is performed
+                print(event)
+                if event.type == "QUIT":  # or if isinstance(event, tcod.event.Quit):
+                    raise SystemExit()
+
+
+if __name__ == "__main__":
+    main()
