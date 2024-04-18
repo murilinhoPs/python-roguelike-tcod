d1bc7f116e8fb72c7039bbf426ec4c77a45e5955
murilinhops <murilinhorps@gmail.com>
2024-04-18 16:43:10 -0300

    Format lint

--- main.py
+++ main.py
@@ -10,14 +10,11 @@ from input_handlers import EventHandler
 def main() -> None:
     screen_width = 80
     screen_height = 50
-
     player_x = int(screen_width / 2)
     player_y = int(screen_height / 2)
-
     tileset = tcod.tileset.load_tilesheet(
         "./dejavu10_10_gs_tc.png", 32, 8, tcod.tileset.CHARMAP_TCOD
     )
-
     event_handler = EventHandler()
 
     with tcod.context.new_terminal(
@@ -27,13 +24,11 @@ def main() -> None:
             title="Yet Another Roguelike Tutorial",
             vsync=True,
     ) as context:
-        root_console = tcod.Console(screen_width, screen_height, order="F")
+        root_console = tcod.console.Console(screen_width, screen_height, order="F")
 
         while True:
             root_console.print(x=player_x, y=player_y, string="@")
-
             context.present(root_console)
-
             root_console.clear()
 
             for event in tcod.event.wait():
