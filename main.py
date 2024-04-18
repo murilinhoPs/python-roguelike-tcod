44b3192bf5bbcce9054aba8beb68bc18b0794218
murilinhops <murilinhorps@gmail.com>
2024-04-18 18:31:21 -0300

    Add player and npc entity on screen

--- main.py
+++ main.py
@@ -4,18 +4,20 @@ from __future__ import annotations
 import tcod
 
 from game.actions import MovementAction, EscAction
+from game.entity import Entity
 from game.input_handlers import EventHandler
 
 
 def main() -> None:
     screen_width = 80
     screen_height = 50
-    player_x = int(screen_width / 2)
-    player_y = int(screen_height / 2)
     tileset = tcod.tileset.load_tilesheet(
         "data/dejavu10_10_gs_tc.png", 32, 8, tcod.tileset.CHARMAP_TCOD
     )
     event_handler = EventHandler()
+    player = Entity(int(screen_width / 2), int(screen_height / 2), "@", (255, 255, 255))
+    npc = Entity(int(screen_width / 2 - 5), int(screen_height / 2), "@", (255, 255, 0))
+    entities = {npc, player}
 
     with tcod.context.new_terminal(
             screen_width,
@@ -27,7 +29,9 @@ def main() -> None:
         root_console = tcod.console.Console(screen_width, screen_height, order="F")
 
         while True:
-            root_console.print(x=player_x, y=player_y, string="@")
+            root_console.print(x=player.x, y=player.y, string=player.char, fg=player.color)
+            root_console.print(x=npc.x, y=npc.y, string=npc.char, fg=npc.color)
+
             context.present(root_console)
             root_console.clear()
 
@@ -36,8 +40,7 @@ def main() -> None:
                 if action is None:
                     continue
                 if isinstance(action, MovementAction):
-                    player_x += action.dx
-                    player_y += action.dy
+                    player.move(action.dx, action.dy)
                 elif isinstance(action, EscAction):
                     raise SystemExit()
 
